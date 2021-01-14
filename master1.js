4d7468220d9e54d8a14cb4299143f5e1fb687c8f
ommaraxx2015 <76623269+ommaraxx2015@users.noreply.github.com>
2021-01-14 03:52:07 +0200

    Create master1.js

---
+++ master1.js
@@ -0,0 +1,38 @@
+$(`<img src="sico/z1erruo3ap10.gif" class="fr" style="width:100%;margin-top: 1px;">
+	<a id="des3" href="#" style="border-radius: 24px;width:23%;margin:2px;" class="fl mini fa fa-facebook
+	btn btn-primary"> فيس بوك</a>
+	<a id="des3" href="#" style="border-radius: 24px;width:23%;margin:2px;" class="fl mini fa fa-star
+	btn btn-default"> ميز نفسك</a>
+	<a id="des3" href="#" style="border-radius: 24px;width:27%;margin:2px;" class="fl mini fa fa-graduation-cap
+	btn btn-primary"> قوانين موقع</a>
+	<a id="des3" href="#" style="border-radius: 24px;width:22%;margin:2px;" class="fl mini fa fa-phone
+	btn btn-default"> اتصل بنا</a>
+	<a id="des4" style="border-radius: 24px 24px 24px 24px;width:377px;margin:3px;" class="fl mini&nbsp; fa fa-shield
+	btn btn" target="_blank" href="http://www.chatnsh.com/"> شات آحبــك : اكبر تجمع عربي خليجي⠀</a> `).insertBefore('.nav-tabs');
+	$('.nav-tabs').addClass('fl').css('width','100%');
+	$( "button.btn.btn-primary" ).css( "border-radius", "24px 24px 24px 24px" );
+	$(".border.corner").css("border-radius","24px 24px 24px 24px");
+	$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera" />
+	<link rel="stylesheet" href="https://rawcdn.githack.com/ommaraxx2015/omar_alhaded2021/6eeb7993bc789299d85554a80de9156df478dea8/master1.css">
+	<style>
+	.fitimg.fl.u-pic {box-shadow: 0 1px 1px -1px rgb(0,0,0), 0 0 6px rgb(216,92,140);border-radius: 4px 4px 4px 4px;;}
+	textarea.fl.filw.corner.tbox {border-radius: 10px;padding-left: 5px;border: 2px solid #adadad;margin-top: 2px;}
+	.label.tc.border.btn.fl, .label.tc.border.btn.fl {border: 2px solid #adadad;font-family: 'jazeera',FontAwesome;border-radius: 24px 24px 24px 24px;}
+	
+	</style>`).insertBefore('body');
+$('.hand.nosel.fl.uzr.border').css({'margin': '1px 0',borderRight: '3px solid #005F69',borderLeft: '3px solid #adadad',borderTop: '1px solid #adadad',borderBottom: '1px solid #adadad',paddingTop: '3px',borderRadius: '5px'});
+$('.uzr.fl.corner.borderg').css({'margin': '0px 0',borderRight: '3px solid #005F69',borderLeft: '3px solid #adadad',borderTop: '.40px solid #adadad',borderBottom: '.50px solid #adadad',paddingTop: '3px',borderRadius: '5px'}); 
+$('.room.borderg.hand.nosel.fl').css({'margin': '1px 0',borderRight: '3px solid #005F69',borderLeft: '3px solid #adadad',borderTop: '1px solid #adadad',borderBottom: '1px solid #adadad',paddingTop: '3px',borderRadius: '5px'});
+$(`<div><marquee direction="right" width="100%" id="des1" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5"> &nbsp;اهلا وسهلا بكم في شات <font color="#357524" class="des2"> آحبــك </font>افضل سوبر لهذا الاسبوع<font class="des2" color="#800000"> قريبـا </font>افضل زائر لهذا الاسبوع<font class="des2" color="#ff9900"> قريبـا </font>افضل زائرة لهذا الاسبوع<font class="des2" color="#ff00ff"> قريبـا </font>مبدع الحائط لهذا الاسبوع<font class="des2" color="#5CAD9A"> قريبـا </font>مبدعه الحائط لهذا الاسبوع<font class="des2" color="#800000"> قريبا َ</font>مع تمنياتنا للجميع باطيب الاوقات&nbsp;</marquee></div>`).insertBefore('div#tlogins .lonline.light.break'); 
+$(`<!--<a class="fl fa fa-image btn ui-corner-all ui-shadow ui-btn ui-btn-inline borderg" style="color:purple;margin:2px;width: 106px;text-align: center;font-size:15px!important;"><span dir="rtl"> (0) </span><span style="font-size: 15px!important;>omar alhaded </span></a>-->
+<a href="omar _ alhaded" target="_blank" style="width:107px; margin-right: 5px;
+margin-left: 2px;text-align: center;float: right;border-radius: 5px;
+border-color: #990033;
+padding: 3px;font-family: 'Cairo', sans-serif, Fontawesome;font-size: 13px!important;" class="SLPBeats fab fa-angellist" >Style By:omar alhaded</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+$("[data-target=#wall]").attr('title','  ♥️الحااائط للابـــــداع ♥️          ')
+ $("a.fa.fa-user-plus").text(' Sign Up ');
+ $("a.fa.fa-user").text(' Login ');
+$(".ubnr").remove();
+$('.u-msg.dots').removeClass('dots');
+$(".fa-gear").text("الضبط").css("width","");
+$('.uzr.fl.corner.borderg').css({'margin': '0px 0',borderRight: '0px solid #ffffff',borderLeft: '0px solid #ffffff',borderTop: '.0px solid #ffffff',borderBottom: '.0px solid #ffffff',paddingTop: '3px',borderRadius: '0px 0px 0px 0px'});
